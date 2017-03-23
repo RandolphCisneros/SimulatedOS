e23eade89108653d14244da81cb6d4da239e7546
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-03-23 13:25:13 -0400

    Update OS.cpp
    Added comments

--- OS.cpp
+++ OS.cpp
@@ -1,16 +1,18 @@
 #include "OS.h"
 
+//initialize powerOff to false
 OS::OS() {
 	powerOff = false;
 }
 
 OS::~OS(){}
 
-
+//simple message while it is running. More methods will come in here; this constantly loops and "listens" for interrupts
 void OS::run(){
 	std::cout << "OS is running" << endl;
 }
 
+//This sets poweroff to true. I don't know if I want this outside of run or inside.
 void OS::shutdown(){
 	std:: cout << "OS is shutting down" << endl;
 	powerOff = true;
