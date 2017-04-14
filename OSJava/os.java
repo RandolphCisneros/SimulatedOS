356609ef0d2283928c1ff1290fec1280ff39d1be
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-04-14 19:57:35 -0400

    Adding java folder and classes

---
+++ OSJava/os.java
@@ -0,0 +1,25 @@
+
+public class os {
+	
+	public static void startup(){
+		System.out.println("In startup");
+	}
+
+	public static void Crint(int[]a, int[]p){
+		System.out.println ("In Crint");
+	}
+
+	public static void Dskint (int[]a, int[]p){
+		System.out.println("In Dskint");
+	}
+
+	public static void Drmint (int[]a, int[]p){
+		System.out.println("In Drmint");
+	}
+	public static void Tro (int[]a, int[]p){
+		System.out.println("In Tro");
+	}
+	public static void Svc (int[]a, int[]p){
+		System.out.println("In Svc");
+	}
+}
