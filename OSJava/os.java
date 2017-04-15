96c196505bc494bba992a629ce59df792fadfd60
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-04-14 20:06:22 -0400

    Update os.java
    Added some static variables

--- OSJava/os.java
+++ OSJava/os.java
@@ -1,8 +1,15 @@
 
 public class os {
 	
+
+
+	
+	
 	public static void startup(){
 		System.out.println("In startup");
+		static Stack<Job> processorStack = new Stack<Job>();	//may create a jobqueue class
+		static JTable jobTable = new JTable(Job, jobNumber);		//may arrange this differently
+		static Queue<Job> readyQueue = new ArrayList<Job>();
 	}
 
 	public static void Crint(int[]a, int[]p){
