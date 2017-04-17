521e1801d6b00be427902a5f404a5785c9993b5b
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-04-17 14:24:56 -0400

    Update os.java
    Added initial free space to startup()

--- OSJava/os.java
+++ OSJava/os.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class os {
 
+	public static final int MAX_FREE_SPACE = 100;
 
 	public static void startup(){
 		System.out.println("In startup");
@@ -9,6 +10,10 @@ public class os {
 		Queue<Job> readyQueue = new LinkedList<Job>();
 		LinkedList<Job> jobTable = new LinkedList<Job>();
 		LinkedList<SizeAddressPair> addressTable = new LinkedList<SizeAddressPair>();
+		
+		//This block allocates the new free space in memory and puts it on the address table.
+		SizeAddressPair initialFreeSpace = new SizeAddressPair(1, MAX_FREE_SPACE);
+		addressTable.push(initialFreeSpace);
 	}
 
 	public static void Crint(int[]a, int[]p){
