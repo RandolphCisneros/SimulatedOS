85c10d0528f6174cc1e9c2cc93d4e278a5b9263b
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-04-19 15:20:55 -0400

    Update SizeAddressTable.java
    Added some functions to sizeAddressTAble

--- OSJava/SizeAddressTable.java
+++ OSJava/SizeAddressTable.java
@@ -1,4 +1,13 @@
 class SizeAddressTable<E> extends PrioirityQueue<E>{
+  
+  
+  public SizeAddressPair findFreeSpace(){
+    return //return largest size pair
+  }
+  
+  public void assignJob(Job newJob){
+  //add job to address space?
+  }
 
 
 
