11c78077e420c7f0c9665a99f754a0563aefc936
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-04-24 09:33:53 -0400

    Add files via upload
    Re uploading lost files

---
+++ OSJava/SizeAddressPair.java
@@ -0,0 +1,30 @@
+//This is a simple pair class to be used in the FreeSpaceTable, organized by the WORST FIT 
+//algorithm. Hence, size comes first.	-R. Cisneros
+public class SizeAddressPair implements Comparable<SizeAddressPair> {
+	
+	private int size;
+	private int address;
+	
+	SizeAddressPair(int sz, int a){
+		size = sz;
+		address = a;
+	}
+	//accessors and mutators	-R.Cisneros
+	public int getSize(){return size;}
+	public int getAddress(){return address;}
+	public void setSize(int sz){size = sz;}
+	public void setAddress(int a){address = a;}
+	
+	//I implemented Comparable for sorting purposes. Just learned this in OOP Class -R. Cisneros
+	//*****TRACE LATER AND MAKE SURE ITS RIGHT!!!!!!!
+	public int compareTo(SizeAddressPair sAP){
+		if(size == sAP.getSize()){
+			if(address == sAP.getAddress())
+				return 0;
+			return address - sAP.getAddress();
+		}
+		return size - sAP.getSize();
+	}
+	
+}
+	
