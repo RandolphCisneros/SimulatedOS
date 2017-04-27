88ec3c979bc84d4de792f46f639c91cd27270ae8
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-04-27 12:31:49 -0400

    Update os.java
    switched siodrum direction

--- OSJava/os.java
+++ OSJava/os.java
@@ -36,14 +36,15 @@ public class os {
 		System.out.println ("In Crint");
 		Job newestJob = new Job(p[1],p[2],p[3],p[4],p[5]);																		//1. Job arrives. We take the parameters.
 		if (addressTable.assignJob(newestJob)){																						//2a. addressTable checks if there's enough free space. If there is it gets allocated free space and put on the readyqueue
-			sos.siodrum(newestJob.getJobNumber(), newestJob.getJobSize(), newestJob.getJobAddress(), 0);		//3a. Don't know if I should do this with siodrum. Puts job on core (memory)
+			sos.siodrum(newestJob.getJobNumber(), newestJob.getJobSize(), newestJob.getJobAddress(), 1);		//3a. Don't know if I should do this with siodrum. Puts job on core (memory)
 			readyQueue.add(newestJob);
 			System.out.println("Job address: " + newestJob.getJobAddress());
 			System.out.println("Job size: " + newestJob.getJobSize());
+			System.out.println("Job is addressed correctly");
 		}
 		else{
 			waitingQueue.add(newestJob);																								//2b. If not, then it gets put on the waitingQueue.
-			sos.siodrum(newestJob.getJobNumber(), newestJob.getJobSize(), newestJob.getJobAddress(), 1);		//3b. Don't know if I should do this with siodrum. Puts job on backing store
+			sos.siodrum(newestJob.getJobNumber(), newestJob.getJobSize(), newestJob.getJobAddress(), 0);		//3b. Don't know if I should do this with siodrum. Puts job on backing store
 		}
 		jobTable.add(newestJob);																											//4 Push onto jobTable
 		dispatcher(a, p);
