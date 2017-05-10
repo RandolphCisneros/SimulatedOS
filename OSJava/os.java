72807f89907d3fe3a99c256f942ec8e0a010fc99
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-05-10 09:35:30 -0400

    Update os.java
    Added jobNumber comparison to Dskint

--- OSJava/os.java
+++ OSJava/os.java
@@ -99,7 +99,7 @@ public class os {
 			jobCompletingIO.setBlockFlag(false);	//4. Set the blockFlag to false.
 			readyQueue.add(jobCompletingIO);	//5. Put on readyQueue. If a job wasn't blocked, it is already on the queue.
 		}
-		if (jobCompletingIO == jobToRun){
+		if (jobCompletingIO.getJobNumber() == jobToRun.getJobNumber()){
 			jobToRun.setBlockFlag(false);
 			System.out.println("IOFLAG: " + jobToRun.getIOFlag());
 		}
