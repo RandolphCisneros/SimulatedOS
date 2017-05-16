3474446ad1b9f407f92af4bf7168fd168003fe61
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-05-16 11:49:20 -0400

    Update SizeAddressTable.java
    Added findShortestTimeSlice()

--- OSJava/SizeAddressTable.java
+++ OSJava/SizeAddressTable.java
@@ -9,6 +9,7 @@ class SizeAddressTable {
   LinkedList<SizeAddressPair> freeSpaceList = new LinkedList<SizeAddressPair>();//list that tracks remaining free spaces
   LinkedList<Job> jobsAddressed = new LinkedList<Job>();	//List of jobs to track the job addresses
   private static Job largestJob;
+  private static int shortestTimeSlice;
   
   //constructor initializes largest free space and adds it to the table.
   //I used a default constructor here since it will only be used once in the OS
@@ -67,8 +68,7 @@ class SizeAddressTable {
 			if((completedJobSize + completedJobAddress) == current.getAddress()){	//check for free space after the job
 				System.out.println("Found free adjacent free space in the back " + current.getAddress() + " " + current.getSize());
 				completedJobSize += current.getSize();
-				freeSpaceList.remove(current);
-				
+				freeSpaceList.remove(current);	
 			}
 		}
 		System.out.println("Checking for adjacent free space in the front");
@@ -92,17 +92,17 @@ class SizeAddressTable {
   }
   
   //After a job has been assigned, this function will be used to find and assign the new largest remaining free space
-  public void findNewLargestRemainingFreeSpace(){
-	  System.out.println("Printing all free spaces");
-	for (int i = 0; i < freeSpaceList.size(); i++){				//1. Iterate through freeSpaceList
-		SizeAddressPair current = freeSpaceList.get(i);			//2. Create pointer to object
-		System.out.println(current.getAddress() + " " + current.getSize());
-		if (current.getSize() > largestRemainingFreeSpace.getSize()){	//3. Compare sizes. If current is largest, re-assign largest remaining free space
-			largestRemainingFreeSpace = current;
-		}
+ 	 public void findNewLargestRemainingFreeSpace(){
+		System.out.println("Printing all free spaces");
+		for (int i = 0; i < freeSpaceList.size(); i++){				//1. Iterate through freeSpaceList
+			SizeAddressPair current = freeSpaceList.get(i);			//2. Create pointer to object
+			System.out.println(current.getAddress() + " " + current.getSize());
+			if (current.getSize() > largestRemainingFreeSpace.getSize()){	//3. Compare sizes. If current is largest, re-assign largest remaining free space
+				largestRemainingFreeSpace = current;
+			}
 		System.out.println("Largest Remaining Free Space: " + largestRemainingFreeSpace.getAddress() + " " + largestRemainingFreeSpace.getSize());
-	}
-  }
+		}
+  	}
 	
 	public void findNewLargestJob(){
 		for (int i = 0; i < jobsAddressed.size(); i++){
@@ -114,5 +114,22 @@ class SizeAddressTable {
 		System.out.println("Largest Job is : " + largestJob.getJobNumber() + largestJob.getJobAddress() + largestJob.getJobSize());
 	}
 	
+	public void findShortestTimeSlice(){
+		if(!jobsAddressed.isEmpty()){
+			Job shortestJob = jobsAddressed.get(0);
+			int minimumTimeSlice = shortestJob.getMaxCpuTime / 4;
+			if(jobsAddressed.size() > 1){
+				for (int i = 1; i < jobsAddressed.size(); i++){
+					Job current = jobsAddressed.get(i);
+					int currentTimeSlice = jobsAddressed.getMaxCpuTime() / 4;
+					if(currentTimeSlice < minimumTimeSlice){
+						minimumTimeSlice = currentTimeSlice;
+					}
+				}
+			}
+			shortestTimeSlice = minimumTimeSlice;
+		}
+	}
+			
 	public Job getLargestJob(){return largestJob;}
 }
