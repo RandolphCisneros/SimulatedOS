646e224c05614c1a2f1ca1b572dab83de42f6894
rndcsnrs08 <rndcsnrs08@gmail.com>
2017-05-21 12:50:05 -0400

    Update SizeAddressPair.java
    Removed unnecessary code

--- OSJava/SizeAddressPair.java
+++ OSJava/SizeAddressPair.java
@@ -1,6 +1,6 @@
 //This is a simple pair class to be used in the FreeSpaceTable, organized by the WORST FIT 
 //algorithm. Hence, size comes first.	-R. Cisneros
-public class SizeAddressPair /*implements Comparable<SizeAddressPair>*/ {
+public class SizeAddressPair {
 	
 	private int size;
 	private int address;
@@ -15,16 +15,5 @@ public class SizeAddressPair /*implements Comparable<SizeAddressPair>*/ {
 	public void setSize(int sz){size = sz;}
 	public void setAddress(int a){address = a;}
 	public void setPair(int sz, int a) {size = sz; address = a;}
-	
-	/*//I implemented Comparable for sorting purposes. Just learned this in OOP Class -R. Cisneros
-	//*****TRACE LATER AND MAKE SURE ITS RIGHT!!!!!!!
-	public int compareTo(SizeAddressPair sAP){
-		if(size == sAP.getSize()){
-			if(address == sAP.getAddress())
-				return 0;
-			return address - sAP.getAddress();
-		}
-		return size - sAP.getSize();
-	}*/
 	
 }
